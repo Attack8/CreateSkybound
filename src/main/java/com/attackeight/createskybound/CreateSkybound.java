8c537a41d6cb7026a1b6c4503e2ca3133f5c2216
NerdsOfAFeather <ftcteam18218@gmail.com>
2023-06-06 15:20:48 -0400

    Just up to date on a few things Villager start, made changelog.txt

--- src/main/java/com/attackeight/createskybound/CreateSkybound.java
+++ src/main/java/com/attackeight/createskybound/CreateSkybound.java
@@ -2,9 +2,15 @@ package com.attackeight.createskybound;
 
 import com.mojang.logging.LogUtils;
 import com.tterrag.registrate.Registrate;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.minecraft.world.entity.npc.VillagerTrades;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -13,6 +19,8 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
+
+import java.util.List;
 
 @Mod(CreateSkybound.MODID)
 public class CreateSkybound {
@@ -28,13 +36,16 @@ public class CreateSkybound {
 
         modEventBus.addListener(this::commonSetup);
         ModItems.register();
+        ModVillagers.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-
+        event.enqueueWork(() -> {
+            ModVillagers.registerPOIs();
+        });
     }
 
     @SubscribeEvent
@@ -48,6 +59,22 @@ public class CreateSkybound {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
 
+        }
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID)
+    public static class Events {
+        @SubscribeEvent
+        public static void addCustomTrades(VillagerTradesEvent e) {
+            if (e.getType() == ModVillagers.WANDERING_ENCHANTER.get()) {
+                Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = e.getTrades();
+                ItemStack stack = new ItemStack(ModItems.ASH.get(), 2);
+                int villagerLevel = 1;
+
+                trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                        new ItemStack(Items.EMERALD, 1),
+                        stack, 10,8,0.02f));
+            }
         }
     }
 }
---
+++ src/main/java/com/attackeight/createskybound/ModVillagers.java
@@ -0,0 +1,46 @@
+package com.attackeight.createskybound;
+
+import com.attackeight.createskybound.CreateSkybound;
+import com.google.common.collect.ImmutableSet;
+import net.minecraft.client.tutorial.Tutorial;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.ai.village.poi.PoiType;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class ModVillagers {
+    public static final DeferredRegister<PoiType> POI_TYPES =
+            DeferredRegister.create(ForgeRegistries.POI_TYPES, CreateSkybound.MODID);
+    public static final DeferredRegister<VillagerProfession> VILLAGER_PROFESSIONS =
+            DeferredRegister.create(ForgeRegistries.VILLAGER_PROFESSIONS, CreateSkybound.MODID);
+
+    public static final RegistryObject<PoiType> ENCHANTER_POI = POI_TYPES.register("enchanter_poi",
+            () -> new PoiType(ImmutableSet.copyOf(Blocks.ENCHANTING_TABLE.getStateDefinition().getPossibleStates()),
+                    1,1));
+
+    public static final RegistryObject<VillagerProfession> WANDERING_ENCHANTER = VILLAGER_PROFESSIONS.register("wandering_enchanter",
+            () -> new VillagerProfession("wadering_enchanter", x -> x.get() == ENCHANTER_POI.get(),
+                    x -> x.get() == ENCHANTER_POI.get(), ImmutableSet.of(), ImmutableSet.of(),
+                    SoundEvents.ENCHANTMENT_TABLE_USE));
+
+    public static void registerPOIs() {
+        try {
+            ObfuscationReflectionHelper.findMethod(PoiType.class,
+                    "registerBlockStates", PoiType.class).invoke(null, ENCHANTER_POI.get());
+        } catch (InvocationTargetException | IllegalAccessException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public static void register(IEventBus eventBus){
+        POI_TYPES.register(eventBus);
+        VILLAGER_PROFESSIONS.register(eventBus);
+    }
+}
