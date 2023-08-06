bac29cf03411416f49eb39032cbe9d67598394b6
attackeight <attackeight@gmail.com>
2023-08-06 11:32:11 -0400

    Add old Skyblock Recipes - Mod v0.1.1

---
+++ src/main/java/com/attackeight/createskybound/block/RegeneratingOreBlock.java
@@ -0,0 +1,12 @@
+package com.attackeight.createskybound.block;
+
+import net.minecraft.world.level.block.Block;
+
+public class RegeneratingOreBlock extends Block {
+
+    public RegeneratingOreBlock(Properties pProperties) {
+        super(pProperties);
+    }
+
+
+}
